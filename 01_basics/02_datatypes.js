caa97ded475ca24bec7cb147769b882fb2f17a4f
OA-Biswajit <112506358+OA-Biswajit@users.noreply.github.com>
2023-08-11 07:55:47 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not brouser
+
+console.log("hello");
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("hitesh");
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+// Number => 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefine => value not assigned
+// symbol => unique
+// object
+console.log(typeof "hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
