5f3349c7b3c5a315d70171e66a39d30c8eb09f69
OA-Biswajit <112506358+OA-Biswajit@users.noreply.github.com>
2023-08-11 17:50:23 +0000

    Operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,7 +23,50 @@
 // "" => false
 // "hitesh"=> true
 
-let someNumber =33
-let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber =33
+// let stringNumber =String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****************Operators*****************
+
+
+// let value =3
+// let newValue = -value
+// console.log(newValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+// let str1="hello"
+// let str2="Hitesh"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log("1"+"2");
+// console.log(1+2+"2");
+
+// console.log((3+4)*6%8);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+// console.log(-true);
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+
+// let gameCounter=100
+// gameCounter++
+// console.log(gameCounter);
+
+// let x2 = 3n;
+// const y2 = ++x2;
+// // x2 is 4n; y2 is 4n
