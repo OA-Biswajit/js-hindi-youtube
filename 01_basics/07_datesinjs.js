7dac705f76497dbc4a7f06ad04629688ca508ce4
OA-Biswajit <112506358+OA-Biswajit@users.noreply.github.com>
2023-08-12 12:27:23 +0000

    dates in js

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,47 @@
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+// const options = { timeZone: 'Asia/Kolkata', timeStyle: 'medium' };
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString('en-IN',options));
+
+
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate= new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+// myCreatedDate= new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleDateString());
+
+// let myCreatedDate= new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate= new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate= new Date("01-14-2023")
+// // let myTimeStamps =Date.now()
+// // console.log(myTimeStamps);
+
+// console.log(Math.floor(Date.now()/1000));
+// console.log(myCreatedDate.getTime());
+
+// let newDate=new Date()
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+// console.log(newDate.getSeconds());
+
+// `${newDate.getDay()} and the time `
+
+// newDate.toLocaleString("default",{
+//     weekday:"long",
+//     timeZone:""
+// })
