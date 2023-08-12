5c5fae702ccf7997d942691110700c4150014bf1
OA-Biswajit <112506358+OA-Biswajit@users.noreply.github.com>
2023-08-12 04:45:13 +0000

    Primitive and NonPrimitive datatype

--- 01_basics/datatype_summery.js
+++ 01_basics/datatype_summery.js
@@ -2,40 +2,63 @@
 
 // 7 types : string, Number, Boolean,null,undefined,symbol,BigInt
 
-const score=100
-const scoreValue=100.3
+// const score=100
+// const scoreValue=100.3
 
-const isLoggedIn =false
-const outsideTemp= null
+// const isLoggedIn =false
+// const outsideTemp= null
 
-let userEmail
+// let userEmail
 
-const id=Symbol("123")
-const anotherId=Symbol("123")
+// const id=Symbol("123")
+// const anotherId=Symbol("123")
 
-console.log(id == anotherId);
+// console.log(id == anotherId);
 
-const bigNumber =12763283242342342343249291n
-console.log(typeof bigNumber);
+// const bigNumber =12763283242342342343249291n
+// console.log(typeof bigNumber);
 
 // Reference (Non primitive)
 
 // Array ,Objects ,Functions
 
-const heros =["shaktiman","naagraj","doga"]
-let myObj ={
-    name:"hitesh",
-    age:22,
+// const heros =["shaktiman","naagraj","doga"]
+// let myObj ={
+//     name:"hitesh",
+//     age:22,
+// }
+
+// const myFunction = function(){
+
+//      console.log("Hello world")
+// }
+
+// console.log(typeof outsideTemp);
+
+// console.log(typeof myFunction);
+// console.log(typeof heros);
+// console.log(typeof anotherId);
+
+// ********************************************************
+
+// Stack (Primitive), Heap (Non-Primitive) 
+
+let myYoutubeName ="hiteshchoudharydotcom" 
+let anothername =myYoutubeName
+
+anothername ="chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anothername);
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
 }
 
-const myFunction = function(){
+let userTwo =userOne
 
-     console.log("Hello world")
-}
+userTwo.email="hitesh@google.com"
 
-console.log(typeof outsideTemp);
-
-console.log(typeof myFunction);
-console.log(typeof heros);
-console.log(typeof anotherId);
-
+console.log(userTwo.email);
+console.log(userOne.email);
